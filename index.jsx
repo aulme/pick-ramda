553ce85bd43f7b4463ccfabe49ecb68af3ff9e36
Aleksandrs Ulme <aleksandrs.ulme@gmail.com>
2016-08-05 00:37:45 +0800

    A really crude UI

--- index.jsx
+++ index.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import {render} from 'react-dom';
 import pick from './pick'
+import R from 'ramda'
+
+window.pick = pick
+window.R = R
 
 class App extends React.Component {
   render () {
     return (
       <div>
-        <p> Hello React!</p>
+        <p> {pick(R, [1, 2], 3)} </p>
       </div>
     );
   }
--- public/bundle.js
+++ public/bundle.js
@@ -61,6 +61,10 @@
 	
 	var _pick2 = _interopRequireDefault(_pick);
 	
+	var _ramda = __webpack_require__(/*! ramda */ 176);
+	
+	var _ramda2 = _interopRequireDefault(_ramda);
+	
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 	
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -68,6 +72,9 @@
 	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 	
 	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+	
+	window.pick = _pick2.default;
+	window.R = _ramda2.default;
 	
 	var App = function (_React$Component) {
 	  _inherits(App, _React$Component);
@@ -87,7 +94,9 @@
 	        _react2.default.createElement(
 	          'p',
 	          null,
-	          ' Hello React!'
+	          ' ',
+	          (0, _pick2.default)(_ramda2.default, [1, 2], 3),
+	          ' '
 	        )
 	      );
 	    }
