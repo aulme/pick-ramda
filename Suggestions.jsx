4f694efeb805b9464011c3b1222c039c846b4463
Aleksandrs Ulme <aleksandrs.ulme@gmail.com>
2016-08-06 22:06:51 +0800

    remove logging

--- Suggestions.jsx
+++ Suggestions.jsx
@@ -9,10 +9,6 @@ window.pick = pick
 
 const Output = ({inputs, output}) => {
   const result = pick(R, inputs, output)
-  console.log("************************************************************************")
-  console.log("inputs =" + JSON.stringify(inputs))
-  console.log("output =" + JSON.stringify(output))
-  console.log("result =" + JSON.stringify(result))
 
   return (<TextField
     disabled={true}
