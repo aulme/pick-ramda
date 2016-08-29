d01ea0bc52c99b532041a5bb130a775462ad1be7
Aleksandrs Ulme <aleksandrs.ulme@gmail.com>
2016-08-29 22:32:49 +0800

    and now it doesnt break

--- webpack.config.js
+++ webpack.config.js
@@ -11,11 +11,6 @@ var config = {
     filename: 'bundle.js'
   },
   plugins: [
-  new webpack.DefinePlugin({
-    'process.env': {
-      'NODE_ENV': JSON.stringify('production')
-    }
-  })
 ],
   module: {
     loaders: [
